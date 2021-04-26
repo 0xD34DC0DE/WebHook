1a03811f733f8e464bc9b78c96fb5495fb129447
William Jean Mireault <williamjeanmireault@gmail.com>
2021-04-26 15:07:53 -0400

    Made simple pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private LevelManager _levelManager;
+    private bool _isPaused;
 
     private void Start()
     {
@@ -10,6 +11,36 @@ public class GameManager : Singleton<GameManager>
         EventManager.Listen("OnSceneLoaded", OnSceneUnloaded);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (_isPaused)
+            UnpauseGame();
+        else
+            PauseGame();
+        
+        EventManager.Invoke("TogglePause");
+    }
+
+    private void UnpauseGame()
+    {
+        _isPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    private void PauseGame()
+    {
+        _isPaused = true;
+        Time.timeScale = 0f;
+    }
+    
     private void OnSceneLoaded()
     {
         print("Scene loaded");
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,13 @@
+﻿public class UIManager : Singleton<UIManager>
+{
+    void Start()
+    {
+        EventManager.Listen("TogglePause", togglePauseMenu);
+        gameObject.SetActive(false);
+    }
+
+    private void togglePauseMenu()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
